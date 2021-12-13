ac0bd1686210b08ec399d122d24825c5886f26ac
Fer Guevara <kafera.g10@gmail.com>
2021-12-13 13:59:17 -0600

    Codificación
    Se creo el desarrollo del sistema

---
+++ index.js
@@ -0,0 +1,14 @@
+function loadContent(){
+    let ul = document.createElement("ul");
+
+    axios.get("https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8").then((response) => {
+        for(let content of response.data){
+            let li = document.createElement("li");
+            li.innerHTML = content.name + " vive en " + content.city + "<br>";
+            ul.appendChild(li);
+        }
+
+        let div = document.getElementById("content");
+        div.appendChild(lista);
+    });
+}
